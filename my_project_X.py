cbfc8bf84f8b8db1250b426c3f8cdd889ed7285a
Arsemon4ik <misterarsemon4ik@gmail.com>
2021-03-20 17:54:49 +0200

    Add my game to Git

---
+++ my_project_X.py
@@ -0,0 +1,19 @@
+
+from random import random
+# Крестики-нолики
+
+
+Matrix = []
+
+for i in range(3):
+        Matrix.append('| |')
+print("Matrix: \n")
+for k in range(i+1):
+    print(Matrix)
+print ("\n")
+flag = True
+i=1
+while flag:
+    print("Ход:",i,"\n")
+    flag = False
+
