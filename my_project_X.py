202f708f1d138cd973662b0819b6f88b6bda59e5
Arsemon4ik <76693557+Arsemon4ik@users.noreply.github.com>
2021-03-22 14:17:34 +0200

    update my game (my work)

--- my_project_X.py
+++ my_project_X.py
@@ -1,19 +1,109 @@
+print("My game attempt #2\n")
 
-from random import random
-# Крестики-нолики
+field = [[" "] * 3 for i in range(3)]
+counter = 0
 
 
-Matrix = []
+def Show():
+    print("-----------------")
+    print(f"|x/y| 0 | 1 | 2 |")
+    for i in range(3):
+        print(f"| {i} | {field[i][0]} | {field[i][1]} | {field[i][2]} |")
+    print("-----------------")
 
-for i in range(3):
-        Matrix.append('| |')
-print("Matrix: \n")
-for k in range(i+1):
-    print(Matrix)
-print ("\n")
-flag = True
-i=1
-while flag:
-    print("Ход:",i,"\n")
-    flag = False
+def Ask():
+    while True:
+        x, y = map(int,input("Сделайте ход(x,y): ").split())
+
+        if (0<= x <=2 and 0<= y <=2):
+            if field[x][y] == " ":
+                return x,y
+                break
+            else:
+                print("Ячейка занята!")
+        else:
+            print("Вы вышли из диапазона! 0-2")
+
+
+def checkwin():
+    for i in range(3):
+        WinSymbol = []
+        for j in range(3):
+            WinSymbol.append(field[i][j])
+        if WinSymbol == ["X", "X", "X"]:
+            print("Выиграл X!")
+            return True
+
+        elif WinSymbol == ["O", "O", "O"]:
+            print("Выиграл O!")
+            return True
+
+
+ 
+    for i in range(3):
+        WinSymbol = []
+        for j in range(3):
+            WinSymbol.append(field[j][i])
+        if WinSymbol == ["X", "X", "X"]:
+            print("Выиграл X!")
+            return True
+
+        elif WinSymbol == ["O", "O", "O"]:
+            print("Выиграл O!")
+            return True
+
+
+    WinSymbol = []
+    for i in range(3):
+
+        WinSymbol.append(field[i][2-i])
+        if WinSymbol == ["X", "X", "X"]:
+            print("Выиграл X!")
+            return True
+
+        elif WinSymbol == ["O", "O", "O"]:
+            print("Выиграл O!")
+            return True
+
+    WinSymbol = []
+    for i in range(3):
+        WinSymbol.append(field[i][i])
+        if WinSymbol == ["X", "X", "X"]:
+            print("Выиграл X!")
+            return True
+
+
+        elif WinSymbol == ["O", "O", "O"]:
+            print("Выиграл O!")
+            return True
+
+
+    return False
+
+
+
+while True:
+    Show()
+    if counter % 2 == 0:
+        print("Ходит Х: ")
+    else: 
+        print("Ходит О: ")
+
+    x,y = Ask()
+
+    if counter % 2 == 0:
+        field[x][y] = 'X'
+    else:
+        field[x][y] = 'O'
+    if checkwin():
+        Show()
+        break
+       
+
+    counter +=1
+    if counter >= 9:
+        print("Ничья!")
+        break
+
+
 
